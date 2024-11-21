d664e3309781df7f5eaa636d25a771dbb079083e
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 22:16:05 +0600

    UI: designing Hero Section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Home Page</h1>
+      <h1 className="text-2xl text-center">
+        <HeroSection />
+      </h1>
     </main>
   );
 };
