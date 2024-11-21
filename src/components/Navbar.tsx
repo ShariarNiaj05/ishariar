c08aed818a335a0f17785c0434c6001924649171
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 22:02:24 +0600

    UI: designing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,6 +13,15 @@ const Navbar = ({ className }: { className?: string }) => {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
+        <Link href={"/"}>
+          {" "}
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+
         <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/web-dev">Web Development</HoveredLink>
