b85574d7cc70722e6ba75167d906aec282099861
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 21:30:47 +0600

    UI: designing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/lib/utils";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 
 const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+
   return (
     <div className="relative w-full flex items-center justify-center">
       <Navbar className="top-2" />
