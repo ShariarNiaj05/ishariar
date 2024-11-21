6812fe8e092cc6c8d3b32f234565a01b47947b42
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 18:33:25 +0600

    adding necessary page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world! .............</h1>
+    <h1 className="text-3xl font-bold text-center">
+      Hello world! .............
+    </h1>
   );
 }
