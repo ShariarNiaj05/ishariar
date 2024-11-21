14b55fdd09cbe9e9134b8b696aec59f5971b4612
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 21:32:24 +0600

    UI: designing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,9 @@ const Navbar = ({ className }: { className?: string }) => {
   const [active, setActive] = useState<string | null>(null);
 
   return (
-    <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-2" />
-      <p className="text-black dark:text-white">
-        The Navbar will show on top of the page
-        <HoveredLink />
-      </p>
-    </div>
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    ></div>
   );
 };
 
