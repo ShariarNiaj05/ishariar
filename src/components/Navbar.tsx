0f5c72736f563a9fe0697441a71b338739a35897
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 21:33:11 +0600

    UI: designing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,9 @@ const Navbar = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    ></div>
+    >
+      nav
+    </div>
   );
 };
 
