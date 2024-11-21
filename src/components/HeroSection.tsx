16c632bc33639e9861aea54a5691d800c2fa283e
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 00:16:19 +0600

    UI: designing Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       {" "}
       <Spotlight
         className="-top-90 left-10 md:left-60 md:-top-20"
-        fill="white"
+        fill="#c2a7ae"
       />
       <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
         <div className="p-4 relative z-10 w-full text-center">
