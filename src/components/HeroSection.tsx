a05de8b1c262b91131c4e35baf966a56246eb271
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 23:22:24 +0600

    UI: designing Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,9 @@ const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
-        <h1>Full-Stack Developer with Digital Visionary</h1>
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral=400">
+          Full-Stack Developer with Digital Visionary
+        </h1>
         <p>
           Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
           Visionary Perspective
