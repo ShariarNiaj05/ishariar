e3336954d15bba4b33085ad7310f6073ff27791c
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 00:04:20 +0600

    UI: designing Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,25 +3,28 @@ import { Spotlight } from "./ui/Spotlight";
 
 const HeroSection = () => {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <>
+      {" "}
       <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
+        className="-top-70 left-0  md:block md:left-60 md:-top-20"
         fill="white"
       />
-      <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700 ">
-          Full-Stack Developer with Digital Visionary
-        </h1>
-        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
-          Visionary Perspective
-        </p>
-        <div className="mt-4">
-          <Link href={"/"}>Github</Link>
-          <Link href={"/"}>Resume</Link>
+      <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <div className="p-4 relative z-10 w-full text-center">
+          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700 ">
+            Full-Stack Developer with Digital Visionary
+          </h1>
+          <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+            Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
+            Visionary Perspective
+          </p>
+          <div className="mt-4">
+            <Link href={"/"}>Github</Link>
+            <Link href={"/"}>Resume</Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
