1d28ed27c7fecc4dc0ade1730a6e115dec17d1a0
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 19:06:33 +0600

    adding necessary page

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,22 +1,15 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Shariar's Portfolio",
-  description: "All the details of Portfolio.",
-};
-
 export default function CommonLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <>
       {/* <NavBar /> */}
       navbar
       {children}
       footer
       {/* <Footer /> */}
-    </div>
+    </>
   );
 }
