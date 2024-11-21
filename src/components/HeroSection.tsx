788fdd913764095e488f838eca6d456b2c2a963e
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 23:07:11 +0600

    UI: designing Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden">
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       HeroSection HeroSection
     </div>
   );
