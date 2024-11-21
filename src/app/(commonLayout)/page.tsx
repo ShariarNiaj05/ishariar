96280b5cb58f169a6e11e953f41f25c89b3cf097
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-21 23:40:16 +0600

    UI: designing Hero Section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/HeroSection";
 
 const HomePage = () => {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="min-h-screen bg-background/[0.96] antialiased bg-grid-white/[0.02] bg-gradient-to-b from-primary-50">
       <h1 className="text-2xl text-center">
         <HeroSection />
       </h1>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,10 +4,10 @@ const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral=400">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700 ">
           Full-Stack Developer with Digital Visionary
         </h1>
-        <p>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
           Visionary Perspective
         </p>
