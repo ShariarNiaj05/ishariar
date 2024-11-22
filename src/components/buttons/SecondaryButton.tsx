e4916c5236ff17893d640fe0cfa83a260192e07c
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 06:55:28 +0600

    UI: moving border for button

--- src/components/buttons/SecondaryButton.tsx
+++ src/components/buttons/SecondaryButton.tsx
@@ -1,5 +1,14 @@
-const SecondaryButton = () => {
-  return <div></div>;
+import { Button } from "../ui/moving-border";
+
+const SecondaryButton = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Button
+      borderRadius="1.75rem"
+      className="bg-white dark:bg-primary text-black dark:text-black font-bold border-neutral-200 dark:border-secondary"
+    >
+      {children}
+    </Button>
+  );
 };
 
 export default SecondaryButton;
