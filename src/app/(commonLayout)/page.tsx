12bc12938da287ab26d14c8ea7bb6c946e958ec9
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-26 19:02:30 +0600

    UI: adding experience section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,3 +1,4 @@
+import ExperienceSection from "@/components/ExperienceSection";
 import HeroSection from "@/components/HeroSection";
 
 const HomePage = () => {
@@ -5,6 +6,7 @@ const HomePage = () => {
     <main className="min-h-screen bg-background/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-2xl text-center">
         <HeroSection />
+        <ExperienceSection />
       </h1>
     </main>
   );
