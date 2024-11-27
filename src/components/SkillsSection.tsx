7dc4ff049db7b77c31f4018eff7013f8c71f26e2
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-27 22:13:13 +0600

    ui: skills section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,5 +1,12 @@
+import H2Titles from "./Titles/H2Titles";
+
 const SkillsSection = () => {
-  return <div>SkillsSection</div>;
+  return (
+    <section>
+      {" "}
+      <H2Titles>Web Development and Other Skills</H2Titles>
+    </section>
+  );
 };
 
 export default SkillsSection;
