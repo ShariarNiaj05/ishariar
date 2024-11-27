d392e2d305a360c72a46b05340b228327af75226
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-27 22:32:57 +0600

    ui: skills section

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -10,7 +10,7 @@ export const StickyScroll = ({
 }: {
   content: {
     title: string;
-    description: string;
+    description?: string;
     content?: React.ReactNode | any;
   }[];
   contentClassName?: string;
