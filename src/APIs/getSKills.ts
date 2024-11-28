5758678e2b6ab50b7080ac10f9c00622f5bdf541
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-28 19:05:27 +0600

    integration: integrating skills API

--- src/APIs/getSKills.ts
+++ src/APIs/getSKills.ts
@@ -1,3 +1,5 @@
+import { Skills } from "@/types";
+
 export const fetchSkills = async () => {
   try {
     const response = await fetch(
@@ -7,6 +9,20 @@ export const fetchSkills = async () => {
       }
     );
     const { data } = await response.json();
+    const languageSkills = data.filter(
+      (skill: Skills) => skill.category === "Language"
+    );
+    const backendSkills = data.filter(
+      (skill: Skills) => skill.category === "Backend"
+    );
+    const frontendSkills = data.filter(
+      (skill: Skills) => skill.category === "Frontend"
+    );
+    const apiSkills = data.filter((skill: Skills) => skill.category === "APIs");
+
+    const interpersonalSkills = data.filter(
+      (skill: Skills) => skill.category === "Interpersonal"
+    );
     return data?.data;
   } catch (error) {
     console.error("Failed to fetch skills", error);
