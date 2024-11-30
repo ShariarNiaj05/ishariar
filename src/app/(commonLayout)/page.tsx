1e4ff7f316cf576248940551e0f433d6aed84908
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-30 20:31:41 +0600

    UI: customizing banner section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -5,11 +5,11 @@ import SkillsSection from "@/components/SkillsSection";
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-background/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">
+      <div className="text-2xl text-center">
         <HeroSection />
         <ExperienceSection />
         <SkillsSection />
-      </h1>
+      </div>
     </main>
   );
 };
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
 import PrimaryButton from "./buttons/PrimaryButton";
 import SecondaryButton from "./buttons/SecondaryButton";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
@@ -11,23 +12,35 @@ const HeroSection = () => {
         className="-top-90 left-10 md:left-60 md:-top-20"
         fill="#c2a7ae"
       />
-      <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-        <div className="p-4 relative z-10 w-full text-center">
-          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700">
-            Full-Stack Developer with Digital Visionary
-          </h1>
-          <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-            Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
-            Visionary Perspective
-          </p>
-          <div className="mt-4">
-            <Link href={"/"}>
-              <PrimaryButton>Github</PrimaryButton>
-            </Link>
-            <Link href={"/"}>
-              <SecondaryButton>Resume</SecondaryButton>{" "}
-            </Link>
+      <div className="flex">
+        <div className="h-auto md:h-[40rem] w-[60%]rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+          <div className="p-4 relative z-10 w-full text-center">
+            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700">
+              Full-Stack Developer with Digital Visionary
+            </h1>
+            <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+              Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
+              Visionary Perspective
+            </p>
+            <div className="mt-4">
+              <Link href={"/"}>
+                <PrimaryButton>Github</PrimaryButton>
+              </Link>
+              <Link href={"/"}>
+                <SecondaryButton>Resume</SecondaryButton>{" "}
+              </Link>
+            </div>
           </div>
+        </div>
+        <div className="w-[40%]">
+          <Link href={"https://app.daily.dev/shariarislam"}>
+            <Image
+              src="https://api.daily.dev/devcards/v2/VzIq43FTtucx4ciSOg2qa.png?type=default&r=rhq"
+              width="356"
+              height="356"
+              alt="Shariar Islam's Dev Card"
+            />
+          </Link>
         </div>
       </div>
     </>
