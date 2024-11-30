9f2120a671ed6dfb80015260b39e19b030b03b0d
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-30 21:02:54 +0600

    UI: customizing banner section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,14 +6,14 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div>
+    <div className="">
       {" "}
       <Spotlight
         className="-top-90 left-10 md:left-60 md:-top-20"
         fill="#c2a7ae"
       />
-      <div className="h-auto md:h-[40rem] w-full rounded-md flex flex- lg:flex-row items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-        <div className="p-4 relative z-10 w-[1/2] text-center">
+      <div className="h-auto md:h-[20rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto pt-10 md:py-0">
+        <div className="px-4 relative z-10 w-full text-center">
           <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700">
             Full-Stack Developer with Digital Visionary
           </h1>
@@ -31,18 +31,17 @@ const HeroSection = () => {
             </Link>
           </div>
         </div>
-
-        <div className="p-4 relative z-10 w-[1/2] text-center">
-          <Link target="_blank" href={"https://app.daily.dev/shariarislam"}>
-            <Image
-              src="https://api.daily.dev/devcards/v2/VzIq43FTtucx4ciSOg2qa.png?type=wide&r=rhq"
-              width="720"
-              height="720"
-              alt="Shariar Islam's Dev Card"
-              className="mt-3 mx-auto"
-            />
-          </Link>
-        </div>
+      </div>
+      <div className="relative z-10 w-full text-center">
+        <Link target="_blank" href={"https://app.daily.dev/shariarislam"}>
+          <Image
+            src="https://api.daily.dev/devcards/v2/VzIq43FTtucx4ciSOg2qa.png?type=wide&r=rhq"
+            width="720"
+            height="720"
+            alt="Shariar Islam's Dev Card"
+            className="mt-3 mx-auto"
+          />
+        </Link>
       </div>
     </div>
   );
