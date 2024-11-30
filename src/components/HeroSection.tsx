916ade3ce631bd71edd24e1806280c2ad05060bd
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-30 20:48:26 +0600

    UI: customizing banner section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,13 +6,13 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div className="max-w-7xl mx-auto justify-between">
+    <div>
       {" "}
       <Spotlight
         className="-top-90 left-10 md:left-60 md:-top-20"
         fill="#c2a7ae"
       />
-      <div className="h-auto md:h-[40rem] w-[60%]rounded-md flex flex- lg:flex-row items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <div className="h-auto md:h-[40rem] w-full rounded-md flex flex- lg:flex-row items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
         <div className="p-4 relative z-10 w-[1/2] text-center">
           <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700">
             Full-Stack Developer with Digital Visionary
