5514ba51f45e2e83ca206faecc973a648aeb8f2d
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-30 20:37:17 +0600

    UI: customizing banner section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,10 +18,19 @@ const HeroSection = () => {
             <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-700">
               Full-Stack Developer with Digital Visionary
             </h1>
-            <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+            <Link target="_blank" href={"https://app.daily.dev/shariarislam"}>
+              <Image
+                src="https://api.daily.dev/devcards/v2/VzIq43FTtucx4ciSOg2qa.png?type=wide&r=rhq"
+                width="600"
+                height="600"
+                alt="Shariar Islam's Dev Card"
+                className="mt-3 mx-auto"
+              />
+            </Link>
+            {/* <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
               Seamless Solutions, Visionary Execution: Full-Stack Prowess with a
               Visionary Perspective
-            </p>
+            </p> */}
             <div className="mt-4">
               <Link href={"/"}>
                 <PrimaryButton>Github</PrimaryButton>
@@ -32,16 +41,16 @@ const HeroSection = () => {
             </div>
           </div>
         </div>
-        <div className="w-[40%]">
+        {/* <div className="w-[40%]">
           <Link href={"https://app.daily.dev/shariarislam"}>
             <Image
-              src="https://api.daily.dev/devcards/v2/VzIq43FTtucx4ciSOg2qa.png?type=default&r=rhq"
+              src="https://api.daily.dev/devcards/v2/VzIq43FTtucx4ciSOg2qa.png?type=wide&r=rhq"
               width="356"
               height="356"
               alt="Shariar Islam's Dev Card"
             />
           </Link>
-        </div>
+        </div> */}
       </div>
     </>
   );
