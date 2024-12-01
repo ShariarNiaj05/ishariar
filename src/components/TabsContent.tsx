04d64be71d640e5f816009811cc149476b0e50d9
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-01 19:22:54 +0600

    UI: tabs adding for skills section

--- src/components/TabsContent.tsx
+++ src/components/TabsContent.tsx
@@ -1,6 +1,8 @@
+import { fetchSkills, SkillsResponse } from "@/APIs/getSKills";
+import { Skills } from "@/types";
 import Image from "next/image";
 
-const TabsContent = () => {
+const TabsContent = async () => {
   const skillsResponse: SkillsResponse | string | undefined =
     await fetchSkills();
 
@@ -9,33 +11,23 @@ const TabsContent = () => {
   }
   const { frontendSkills } = skillsResponse as SkillsResponse;
 
-  const content =
-    Array.isArray(frontendSkills) && frontendSkills.length > 0
-      ? frontendSkills.map((skill: Skills) => ({
-          title: skill.name, // Set the skill name as the title
-          content: (
-            <div className="h-full w-2xl flex flex-col items-center justify-center text-white">
-              {/* Skill Category */}
-              <p className="mb-4 text-lg font-medium text-neutral-800 dark:text-neutral-200">
-                {skill.category}
-              </p>
-              {/* Skill Media */}
-              {skill.media?.map((mediaItem) => (
-                <Image
-                  key={mediaItem._id}
-                  src={mediaItem.url}
-                  width={300}
-                  height={300}
-                  className="h-32 w-32 object-fit"
-                  alt={skill.name}
-                />
-              ))}
-            </div>
-          ),
-        }))
-      : [];
-
-  return <div></div>;
+  return frontendSkills.map((skill: Skills) => ({
+    content: (
+      <div className="h-full w-2xl flex flex-col items-center justify-center text-white">
+        {/* Skill Media */}
+        {skill.media?.map((mediaItem) => (
+          <Image
+            key={mediaItem._id}
+            src={mediaItem.url}
+            width={300}
+            height={300}
+            className="h-32 w-32 object-fit"
+            alt={skill.name}
+          />
+        ))}
+      </div>
+    ),
+  }));
 };
 
 export default TabsContent;
