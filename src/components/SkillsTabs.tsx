320ed181e34207673125a5b07e40158ac0307b2f
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-01 19:13:04 +0600

    UI: tabs adding for skills section

--- src/components/SkillsTabs.tsx
+++ src/components/SkillsTabs.tsx
@@ -1,47 +1,10 @@
-// "use client";
+"use client";
 
 import Image from "next/image";
 import { Tabs } from "./ui/tabs";
 import LanguageSKills from "./ui/skills/LanguageSKills";
-import FrontendSkills from "./ui/skills/FrontendSkills";
-import { fetchSkills, SkillsResponse } from "@/APIs/getSKills";
-import { Skills } from "@/types";
 
 export const SkillsTabs = async () => {
-  const skillsResponse: SkillsResponse | string | undefined =
-    await fetchSkills();
-
-  if (!skillsResponse) {
-    return <p>Failed to load skills. Please try again later.</p>;
-  }
-  const { frontendSkills } = skillsResponse as SkillsResponse;
-
-  const content =
-    Array.isArray(frontendSkills) && frontendSkills.length > 0
-      ? frontendSkills.map((skill: Skills) => ({
-          title: skill.name, // Set the skill name as the title
-          content: (
-            <div className="h-full w-2xl flex flex-col items-center justify-center text-white">
-              {/* Skill Category */}
-              <p className="mb-4 text-lg font-medium text-neutral-800 dark:text-neutral-200">
-                {skill.category}
-              </p>
-              {/* Skill Media */}
-              {skill.media?.map((mediaItem) => (
-                <Image
-                  key={mediaItem._id}
-                  src={mediaItem.url}
-                  width={300}
-                  height={300}
-                  className="h-32 w-32 object-fit"
-                  alt={skill.name}
-                />
-              ))}
-            </div>
-          ),
-        }))
-      : [];
-
   const tabs = [
     {
       title: "Language",
@@ -49,9 +12,10 @@ export const SkillsTabs = async () => {
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
           <p>Language</p>
-          {/* <LanguageSKills /> */}
+          <LanguageSKills />
           {/* <FrontendSkills /> */}
-          <DummyContent />
+          {/* <DummyContent /> */}
+          {/* {content} */}
         </div>
       ),
     },
