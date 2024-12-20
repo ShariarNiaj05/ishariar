8f290c534e8fe61a119a08965cfd5d77d362ef20
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-20 23:36:38 +0600

    UI: navbar re-design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = ({ className }: { className?: string }) => {
 
   return (
     <nav
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-5 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
